3c8fc089ec055da01803baee157acc316e4f154b
findarip <find.arip@gmail.com>
2022-01-04 18:47:21 +0700

    config sass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <x-navbar></x-navbar>
     {{ $slot }}
     <h1>Welcome Blade</h1>
+    <script src="/js/app.js"></script>p
 </body>
 </html>
